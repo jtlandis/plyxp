bd146c51a4bf5aad4c20c0b129e5932842275e61
jtlandis <jtlandis314@gmail.com>
2024-10-23 03:23:32 -0400

    version bump...

--- R/reexports-SummarizedExperiment.R
+++ R/reexports-SummarizedExperiment.R
@@ -1,46 +1,35 @@
 # The following functions should
 # be available to users from our package
 
-#' @noRd
 #' @export
 SummarizedExperiment::SummarizedExperiment
 
-#' @noRd
 #' @export
 SummarizedExperiment::assay
 
-#' @noRd
 #' @export
 SummarizedExperiment::`assay<-`
 
-#' @noRd
 #' @export
 SummarizedExperiment::assays
 
-#' @noRd
 #' @export
 SummarizedExperiment::`assays<-`
 
-#' @noRd
 #' @export
 SummarizedExperiment::rowData
 
-#' @noRd
 #' @export
 SummarizedExperiment::`rowData<-`
 
-#' @noRd
 #' @export
 SummarizedExperiment::colData
 
-#' @noRd
 #' @export
 SummarizedExperiment::`colData<-`
 
-#' @noRd
 #' @export
 S4Vectors::metadata
 
-#' @noRd
 #' @export
 S4Vectors::`metadata<-`
