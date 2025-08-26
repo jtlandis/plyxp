28389c984a2db21d6e1a527ec28dcd128d4ca042
jtlandis <jtlandis314@gmail.com>
2025-08-26 17:39:41 -0400

    trying to align with dplyr in that results are now grouped if object is grouped

--- R/dplyr-slice.R
+++ R/dplyr-slice.R
@@ -11,7 +11,7 @@ slice.PlySummarizedExperiment <- function(.data, ..., .preserve = FALSE) {
 slice_se_impl <- function(.data, ..., .preserve = FALSE) {
   # we will update .slice_data with some conviencence columns
   # then use that to slice the original data at the end
-  .slice_data <- .data
+  # .slice_data <- .data
   .env <- caller_env()
   quos <- plyxp_quos(..., .ctx_default = "assays", .ctx_opt = c("rows", "cols"))
 
@@ -30,47 +30,33 @@ slice_se_impl <- function(.data, ..., .preserve = FALSE) {
   # to make this function consistent
   # groups <- group_data_se_impl(.data)
   # metadata(.data)[["group_data"]] <- NULL
-  rowData(.slice_data)[[".plyxp:::index"]] <- seq_len(nrow(.slice_data))
-  colData(.slice_data)[[".plyxp:::index"]] <- seq_len(ncol(.slice_data))
-  mask <- new_plyxp_manager.SummarizedExperiment(obj = .slice_data)
+  # rowData(.slice_data)[[".plyxp:::index"]] <- seq_len(nrow(.slice_data))
+  # colData(.slice_data)[[".plyxp:::index"]] <- seq_len(ncol(.slice_data))
+  mask <- new_plyxp_manager.SummarizedExperiment(obj = .data)
   poke_ctx_local("plyxp:::caller_env", .env)
   poke_ctx_local("plyxp:::manager", mask)
   poke_ctx_local("plyxp:::dplyr_verb", "slice")
   mask <- plyxp_evaluate(mask, quos, ctxs, nms, .env)
-  `.plyxp:::index` <- slice_quos <- NULL
-  which_ctx <- NULL
-  if ("rows" %in% ctxs) {
-    ri <- expr(c(!!!syms(nms[ctxs %in% "rows"])))
-    slice_quos <- plyxp_quos(
-      rows(`.plyxp:::index` = {
-        \(x) if (length(x) == 0) 0 else x
-      }(`.plyxp:::index`[!!ri])),
-      .ctx_default = "assays", .ctx_opt = "rows"
-    )
-    which_ctx <- "rows"
-  }
-  if ("cols" %in% ctxs) {
-    ci <- expr(c(!!!syms(nms[ctxs %in% "cols"])))
-    slice_quos <- c(
-      slice_quos,
-      plyxp_quos(
-        cols(`.plyxp:::index` = {
-          \(x) if (length(x) == 0) 0 else x
-        }(`.plyxp:::index`[!!ci])),
-        .ctx_default = "assays", .ctx_opt = "cols"
-      )
-    )
-    which_ctx <- c(which_ctx, "cols")
-  }
-  mask <- plyxp_evaluate(mask, slice_quos, which_ctx, names(slice_quos), .env)
-  results <- mask$result(".plyxp:::index")
+  # we may have empty values, thus we do not collect with
+  # mask$results, instead we will get the values raw and
+  # handle the correct index
+  chops <- mask$apply(
+    .f = function(mask, name) {
+      added <- mask$added
+      names(added) <- added
+      lapply(added, mask$get_chop)
+    },
+    .on_masks = c("rows", "cols")
+  )
+  chops <- flatten_slice_chops(chops, groups = group_data_se_impl(.data))
+  which_ctx <- unique(ctxs)
   switch(length(which_ctx),
     `1` = {
       switch(which_ctx,
-        rows = plyxp_slice_se(.data, results$rows,
+        rows = plyxp_slice_se(.data, chops$rows,
           .preserve = .preserve
         ),
-        cols = plyxp_slice_se(.data, , results$cols,
+        cols = plyxp_slice_se(.data, , chops$cols,
           .preserve = .preserve
         )
       )
@@ -78,10 +64,53 @@ slice_se_impl <- function(.data, ..., .preserve = FALSE) {
     `2` = {
       plyxp_slice_se(
         .data,
-        results$rows[[".plyxp:::index"]],
-        results$cols[[".plyxp:::index"]],
+        chops$rows,
+        chops$cols,
         .preserve = .preserve
       )
     }
   )
+}
+
+flatten_by_slice_expr <- function(ctx_chops, ctx_group) {
+  if (is.null(ctx_group)) {
+    # no groups - ctx_chops is a list off
+    # index exprs
+    do.call("c", ctx_chops)
+  } else {
+    # ctx_chops[[1]] --> expr1
+    # ctx_chops[[2]] --> expr2 ...
+    #
+    # ctx_chops[[1]][...] --> expr1[group1, ..., groupn]
+    chops <- lapply(ctx_chops,
+      function(chop, true_ind) {
+        Map(`[`, true_ind, chop)
+      },
+      true_ind = ctx_group$.indices
+    )
+    lapply(
+      seq_along(ctx_group$.indices),
+      function(i, chops) {
+        do.call("c", lapply(chops, .subset2, i))
+      },
+      chops = chops
+    )
+  }
+}
+
+flatten_slice_chops <- function(chops, groups = NULL) {
+  Map(
+    flatten_by_slice_expr,
+    ctx_chops = chops,
+    ctx_group = groups %||% list(NULL, NULL)
+  ) |>
+    lapply(function(by_expr) {
+      if (is.list(by_expr)) {
+        by_expr <- do.call("c", by_expr)
+      }
+      if (!is.numeric(by_expr)) {
+        stop("`slice` should resolve numeric slices")
+      }
+      by_expr
+    })
 }
