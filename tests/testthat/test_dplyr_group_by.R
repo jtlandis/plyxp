c5945ab69afe70aaf5a0a2015b9a9276422a81ab
jtlandis <jtlandis314@gmail.com>
2025-08-26 15:07:42 -0400

    Update test_dplyr_group_by.R

--- tests/testthat/test_dplyr_group_by.R
+++ tests/testthat/test_dplyr_group_by.R
@@ -44,3 +44,57 @@ test_that("groups can be removed", {
   expect_true("sample" %in% names(group_data(gse4)$col_groups))
   expect_false("condition" %in% names(group_data(gse4)$col_groups))
 })
+
+test_that("groups change on data augment", {
+  gse <- group_by(se_simple, rows(direction), cols(condition))
+  gdata <- group_data(gse)
+  expect_equal(nrow(gdata$row_groups), 2)
+  gdata_ <- group_data(gse[1, ])
+  expect_equal(nrow(gdata_$row_groups), 1)
+  expect_identical(gdata$col_groups, gdata_$col_groups)
+})
+
+test_that("groups change on filter augment", {
+  gse <- group_by(se_simple, rows(direction), cols(condition))
+  gdata <- group_data(gse)
+  expect_equal(nrow(gdata$row_groups), 2)
+  gdata_ <- group_data(filter(gse, rows(direction == "-")))
+  expect_equal(nrow(gdata_$row_groups), 1)
+  expect_identical(gdata$col_groups, gdata_$col_groups)
+  gdata_ <- group_data(filter(gse, rows(direction == "-"), .preserve = TRUE))
+  expect_equal(nrow(gdata_$row_groups), 2)
+  expect_identical(gdata_$row_groups$.indices, list(1:2, integer()))
+  expect_identical(gdata$col_groups, gdata_$col_groups)
+})
+
+test_that("groups change on arrange augment", {
+  gse <- group_by(se_simple, rows(direction), cols(condition))
+  gdata <- group_data(gse)
+  expect_equal(nrow(gdata$row_groups), 2)
+  gdata_ <- group_data(arrange(gse, rows(dplyr::desc(.features))))
+  expect_equal(nrow(gdata_$row_groups), 2)
+  expect_identical(gdata$col_groups, gdata_$col_groups)
+  expect_identical(gdata_$row_groups$.indices, list(c(1L, 3L, 4L), c(2L, 5L)))
+  gdata_ <- group_data(arrange(gse, rows(dplyr::desc(.features)),
+    .by_group = TRUE
+  ))
+  expect_equal(nrow(gdata_$row_groups), 2)
+  expect_identical(gdata_$row_groups$.indices, list(1:3, 4:5))
+  expect_identical(gdata$col_groups, gdata_$col_groups)
+})
+
+
+test_that("groups change on slice augment", {
+  gse <- group_by(se_simple, rows(direction), cols(condition))
+  gdata <- group_data(gse)
+  expect_equal(nrow(gdata$row_groups), 2)
+  gdata_ <- group_data(slice(gse, rows(which(direction == "-"))))
+  expect_equal(nrow(gdata_$row_groups), 1)
+  expect_identical(gdata$col_groups, gdata_$col_groups)
+  gdata_ <- group_data(slice(gse, rows(which(direction == "-")),
+    .preserve = TRUE
+  ))
+  expect_equal(nrow(gdata_$row_groups), 2)
+  expect_identical(gdata_$row_groups$.indices, list(1:2, integer()))
+  expect_identical(gdata$col_groups, gdata_$col_groups)
+})
