eda18147f925eb439329db011f53d225505a20a2
jtlandis <jtlandis314@gmail.com>
2025-09-25 13:42:38 -0400

    Add examples and improve formatting for S7 vector classes

--- R/vctrs-S4-.R
+++ R/vctrs-S4-.R
@@ -1,5 +1,3 @@
-
-
 #' @title S7 classes for vctrs and S4 Vectors
 #' @name vectors
 #' @description
@@ -7,11 +5,19 @@
 #' These classes were made to re-export several `vctrs` functions such that
 #' internals for `plyxp` were consistent with room for optimization.
 #' @seealso [vec_rep()],[vec_recycle()],[vec_slice()]
+#' @examples
+#' # used for defining methods on S7 generics
+#'
+#' S7::method(vec_slice, class_vctrs)
+#' S7::method(vec_slice, class_s4_vctrs)
+#'
 #' @return S7 class union or base class
 #' @export
-class_vctrs <- S7::new_union(S7::class_atomic, S7::class_list,
-                             S7::class_data.frame, S7::class_factor,
-                             S7::class_Date, S7::class_POSIXct)
+class_vctrs <- S7::new_union(
+  S7::class_atomic, S7::class_list,
+  S7::class_data.frame, S7::class_factor,
+  S7::class_Date, S7::class_POSIXct
+)
 
 #' @rdname vectors
 #' @export
