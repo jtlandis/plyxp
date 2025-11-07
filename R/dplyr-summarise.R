c156846237aa5fb2a4bdfa498b9b1e9e4c5abc6d
jtlandis <jtlandis314@gmail.com>
2025-11-07 15:27:08 -0500

    Add tests to ensure vectors are reconstructed correctly!!!

--- R/dplyr-summarise.R
+++ R/dplyr-summarise.R
@@ -89,6 +89,7 @@ summarize_se_impl <- function(.data, ...,
   row_names <- col_names <- NULL
   grouped_rows <- is_grouped_rows(.groups)
   grouped_cols <- is_grouped_cols(.groups)
+  .byrow <- FALSE
   if (grouped_rows || "rows" %in% ctxs) {
     # get all chop data, groups and evaled
     row_chops <- mask_pull_chops(
@@ -102,6 +103,7 @@ summarize_se_impl <- function(.data, ...,
     if (.retain && !grouped_rows) {
       row_chops_sizes <- .nrow <- nrow(.data)
     } else {
+      .byrow <- TRUE
       row_chops_sizes <- .nrow <- 1L
       if (grouped_rows) {
         .nrow <- nrow(.groups$row_groups)
@@ -135,6 +137,7 @@ summarize_se_impl <- function(.data, ...,
     if (.retain && !grouped_cols) {
       col_chops_sizes <- .ncol <- ncol(.data)
     } else {
+      .byrow <- FALSE
       col_chops_sizes <- .ncol <- 1L
       if (grouped_cols) {
         .ncol <- nrow(.groups$col_groups)
@@ -204,7 +207,8 @@ summarize_se_impl <- function(.data, ...,
     map(
       matrix,
       nrow = .nrow,
-      ncol = .ncol
+      ncol = .ncol,
+      byrow = .byrow
     )
 
   out <- SummarizedExperiment(
@@ -227,7 +231,6 @@ summarize_se_impl <- function(.data, ...,
 #' @rdname summarize
 #' @export
 summarise.PlySummarizedExperiment <- summarize.PlySummarizedExperiment
-
 
 
 assert_chops_size <- function(chops, size = 1L) {
